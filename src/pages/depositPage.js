467c24f5e38ca486a8228db724bb700e4640f00a
Vanessa Bautista <vanessawebdev@gmail.com>
2022-12-01 16:37:28 -0800

    added multiple reducers

--- src/pages/depositPage.js
+++ src/pages/depositPage.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 
 const DepositPage = () => {
   const balance = useSelector((state) => state.balanceReducer.balance);
+    const loan = useSelector((state) => state.loanReducer.loan);
   const dispatch = useDispatch();
   const onDepositHandle = () => {
     dispatch({ type: "DEPOSIT", payload: 10 });
@@ -13,6 +14,7 @@ const DepositPage = () => {
       <h1>Deposit</h1>
       <h2>Balance: {balance}</h2>
       <button onClick={onDepositHandle}>Deposit</button>
+        <h2>{loan ? "Loan Applied" : "Loan Needed"}</h2>
     </div>
   );
 };
--- src/pages/withdrawPage.js
+++ src/pages/withdrawPage.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 
 const WithdrawPage = () => {
   const balance = useSelector((state) => state.balanceReducer.balance);
+    const loan = useSelector((state) => state.loanReducer.loan);
   const dispatch = useDispatch();
   const onWithdrawHandle = () => {
     dispatch({ type: "WITHDRAW", payload: 10 });
@@ -13,6 +14,7 @@ const WithdrawPage = () => {
       <h1>Withdraw</h1>
       <h2>Balance: {balance}</h2>
       <button onClick={onWithdrawHandle}>Withdraw</button>
+        <h2>{loan ? "Loan Applied" : "Loan Needed"}</h2>
     </div>
   );
 };
